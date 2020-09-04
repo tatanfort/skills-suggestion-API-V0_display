43ff8e9e5cb2a02d5c393047f281eab77db7ea69
Nathan REDIN <nathan.redin@gmail.com>
2020-09-04 15:34:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,30 +7,11 @@ from flask import request
 import spacy
 import en_core_web_sm
 import operator
-"""
-class Matcher():
-    def __init__(self):
-        #self.nlp_en = "en_core_web_lg"
-        self.nlp_en = en_core_web_sm.load()
-        #self.nlp_fr = spacy.load("fr_core_news_md")
-    def get_top_similarities(self, word, word_list, n, nlp):
-        similarities = {}
-        doc1 = nlp(str(word))
-        for item in word_list:
-                doc2 = nlp(item)
-                if item != word:
-                    similarities[item] = doc1.similarity(doc2)
-                else:
-                    similarities[item] = 1
-        return sorted(similarities.items(),key=operator.itemgetter(1),reverse=True)[:n]
-    
 
-    def get_top_similarities_en(self, word, word_list, n):
-        return self.get_top_similarities(word, word_list, n, self.nlp_en)
- 
 
-matcher = Matcher()
-"""
+
+app = Flask(__name__)
+
 nlp_en = spacy.load("en_core_web_sm")
 
 def Matcher(word, word_list):
@@ -45,9 +26,6 @@ def Matcher(word, word_list):
     return sorted(similarities.items(),key=operator.itemgetter(1),reverse=True)[0][0]
 
 
-app = Flask(__name__)
-
-#test
 n = 10 
 percentage_exploitation = 0.7
 
@@ -58,8 +36,9 @@ top_skills.drop("Unnamed: 0", axis=1, inplace = True)
 top_skills["selection_score"] = [100 for i in range(len(top_skills))]
 top_skills["num_appear"] = [0 for i in range(len(top_skills))]
 
-
 top_skills_grouped = top_skills.groupby(by="job_title")
+
+
 
 @app.route("/")
 def selected_skills_test():
