7ec7383d7937f1324747fa37ffbbb753225e387f
Nathan REDIN <nathan.redin@gmail.com>
2020-09-17 16:22:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -67,23 +67,26 @@ def selected_skills_test2():
     if language == "en":
         job_title = job_title_match_fuzzy(job_title)[0]
         df = top_skills_grouped_en.get_group(job_title).reset_index(drop = True)
-        
-    if nb_skills_selected > len(df):
-        skills = df.top_skills
-    
+       
+    if job_title == None:
+        return "wrong job title"
     else:
-        nb_exploration = round(0.3*nb_skills_selected)
-        nb_exploitation = nb_skills_selected - nb_exploration
+        if nb_skills_selected > len(df):
+            skills = df.top_skills
         
-        #exploration
-        exploration_skills = df.sample(n=nb_exploration).skill
-        #exploitation
-        selection_weight = np.array([1.00001- np.exp(-0.2*x) for x in df.num_appear])*df.selection_score
-        df["weight_sample"] = selection_weight
-        
-        exploitation_skills = df.sample(n = nb_exploitation, weights= "weight_sample").skill
-        
-        skills = exploitation_skills.append(exploration_skills)
-    return skills.reset_index(drop = True).to_json()
+        else:
+            nb_exploration = round(0.3*nb_skills_selected)
+            nb_exploitation = nb_skills_selected - nb_exploration
+            
+            #exploration
+            exploration_skills = df.sample(n=nb_exploration).skill
+            #exploitation
+            selection_weight = np.array([1.00001- np.exp(-0.2*x) for x in df.num_appear])*df.selection_score
+            df["weight_sample"] = selection_weight
+            
+            exploitation_skills = df.sample(n = nb_exploitation, weights= "weight_sample").skill
+            
+            skills = exploitation_skills.append(exploration_skills)
+        return skills.reset_index(drop = True).to_json()
 if __name__ == '__main__':
    app.run(host='127.0.0.1', port=8080, debug=True)
