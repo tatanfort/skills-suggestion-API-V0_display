b5db0232b761f4352134ebeeb020cddafc0de7da
Nathan REDIN <nathan.redin@gmail.com>
2020-09-18 15:48:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def job_title_match_fuzzy(job_searched, threshold = 90):
 
 #french
   
-#job_titles_ROME = pd.read_pickle("job_titles_ROME.pkl")  
+job_titles_ROME = pd.read_pickle("job_titles_ROME.pkl")  
 #spacy.prefer_gpu()
 #nlp = fr_core_news_sm.load()
 
