5e29c5fb6dd85681bfa7f405698558a334f88598
Nathan REDIN <nathan.redin@gmail.com>
2020-09-22 15:00:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def job_title_match_fuzzy(job_searched, threshold = 90):
   
 #job_titles_ROME = pd.read_pickle("job_titles_ROME.pkl")  
 spacy.prefer_gpu()
-nlp = spacy.load("fr_core_news_lg")
+nlp = fr_core_news_lg.load()
 
 def get_top_similarities_fr(word):
     word_list = list(top_skills_fr.job_title.unique())
